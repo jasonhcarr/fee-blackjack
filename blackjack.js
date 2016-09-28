19a81b0811f33db51d16c7bd30a800c18d10f9d2
jasonhcarr <jasonhcarr@gmail.com>
2016-09-28 17:21:05 -0400

    put out the fires but its ugly

--- blackjack.js
+++ blackjack.js
@@ -1,64 +1,56 @@
-
 function runGame() {
-
     var display = document.getElementById('cards');
-    cards = ['A', '2','3', '4','5',"6",'7','7','8', '9', "10", 'J', 'Q', 'K']
+    var cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    var standBtn = document.getElementById('stand').addEventListener('click', function() {checkResult(true, false);});
+    var hitBtn = document.getElementById('hit').addEventListener('click', hit);
+dealCards();
+function dealCards() {
+      var card = Math.floor(Math.random() * cards.length);
+      display.innerHTML = cards[card];
+      card = Math.floor(Math.random() * cards.length);
+      display.innerHTML = display.innerHTML + ' ' + cards[card];
+      checkResult(false, false);
+}
 
     function hit() {
-        var card = Math.round(Math.random() * cards.length);
-        display.innerHTML = cards[card];
-        checkResult('no', true);
+        var card = Math.floor(Math.random() * cards.length);
+        display.innerHTML = display.innerHTML + ' ' + cards[card];
+        checkResult(false, true);
     }
 
-    /**
-     * Check the result of the current cards and alert the game result
-     *
-     * @param  {Boolean} standing  Whether or not the player is standing
-     * @param  {Boolean} hitting   Whether or not the player is hitting
-     * @return {void}
-     */
     function checkResult(standing, hitting) {
-        cards = display.innerHTML.split(' ');
-
+      console.log(standing, hitting);
+        var hand = display.innerHTML.split(' ');
         var cardValue = 0;
-
-        cards.forEach(function (card, i) {
-            if (Number(card)) {
-                cardValue = cardValue + card;
-            }
-
-            if (card === 'J' || card == 'Q' || card === 'J')
-                cardValue = cardValue + 10;
-
-            if (cards[i] = 'A') { cardValue = cardValue += 11; }
+        hand.forEach(function(card) {
+          if (card === 'A') {
+            cardValue = parseInt(cardValue) + 11;
+          }
+          else if (card ===  'J'|| card == 'Q' || card === 'K') {
+              cardValue = parseInt(cardValue) + 10;
+          }
+          else {
+              cardValue = parseInt(cardValue) + parseInt(card);
+        }
+        return cardValue;
         });
-
         if (cardValue < 15 && standing) {
             alert('Dealer wins.');
         }
-        if (cardValue < 18 && standing) {
+        else if (cardValue < 18 && standing) {
             alert('Push!');
         }
-        if (cardValue > 18 & hitting || cardValue === 21) {
+        else if (cardValue > 18 && standing || cardValue === 21) {
             alert('You win!');
         }
-        if (cardValue > 21) {
+        else if (cardValue > 21) {
             alert('You Bust.');
         }
 
-    display.innerHTML = '';
-    card = Math.round(Math.random() * cards.length);
-    display.innerHTML = cards[card];
-    }
 
-    document.getElementById('stand').addEventListener('click', function() {
-        checkREsult(true);
-    });
 
-    document.getElementById('hit').addEventListener('click',function(){checkResult(null, true);});
-
-    card = Math.round(Math.random() * cards.length);
-    display.innerHTML = cards[card];
-    card = Math.round(Math.random() * cards.length);
-    display.innerHTML = display.innerHTML + ' ' + cards[card];
 }
+}
+
+
+runGame();
